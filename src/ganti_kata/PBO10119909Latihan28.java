14bf3eaf8a3308dc113af4f14dc83f5d4da19cf4
anggawanridho <55708748+anggawanridho@users.noreply.github.com>
2020-10-28 13:03:27 +0700

    Add files via upload

---
+++ src/ganti_kata/PBO10119909Latihan28.java
@@ -0,0 +1,33 @@
+package ganti_kata;
+
+import java.util.Scanner;
+
+/**
+ * @author
+ * NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+public class PBO10119909Latihan28 {
+    public static void main (String[] args) {
+        String hasil = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("====Program Mengganti Kata====\n");
+        System.out.print("Masukkan Kalimat : ");
+        String kalimat = scanner.nextLine();
+        System.out.print("Ganti Kata : ");
+        String kata_lama = scanner.nextLine();
+        System.out.print("Menjadi Kata : ");
+        String kata_baru = scanner.nextLine();
+        String[] perkata = kalimat.split(" ");
+        for (String kata : perkata) {
+            if (kata.equalsIgnoreCase(kata_lama)) {
+                hasil += kata_baru + " ";
+            }
+            else {
+                hasil += kata + " ";
+            }
+        }
+        System.out.println("Kalimat Baru : "+hasil.trim());
+    }
+}
